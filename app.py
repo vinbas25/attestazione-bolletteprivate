2758a9680f0bd701f6f15aedfb0cf956187660b7
vinbas25 <vincenzo.basile.unina2@gmail.com>
2025-06-23 17:03:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ def estrai_dati_cliente(testo: str) -> str:
             r'(?:p\.\s*iva|partita\s*iva)\s*[:\-]?\s*([0-9]{11})',
             r'(?:cf|codice\s*fiscale)\s*[:\-]?\s*([A-Z0-9]{16})',
             r'(?:Numero\s*Contatore|Contatore)[\s:]*([0-9]{8,9})',
-            r'(?:Matricola|Serial Number|S/N)[\s:]*([A-Z0-9]{14,15})'
+            r'(?:Matricola|Misuratore|S/N)[\s:]*([A-Z0-9]{14,15})'
         ]
         for pattern in patterns:
             match = re.search(pattern, testo, re.IGNORECASE)

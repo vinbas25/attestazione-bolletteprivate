e8ce2de9cbf0257347669e8cd1bccf41cf3a097e
vinbas25 <vincenzo.basile.unina2@gmail.com>
2025-06-23 20:10:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -201,6 +201,7 @@ def estrai_indirizzo(testo: str) -> str:
             r'Indirizzo\s*fornitura\s*((?:Via|Viale|Piazza|Corso|C\.so|V\.le|Str\.).+?\d{1,5}(?:\s*[A-Za-z]?)?)\b',
             r'(?:Indirizzo|Servizio erogato in|Luogo di fornitura|Indirizzo di fornitura|Indirizzo fornitura)\s*[:\-]?\s*((?:Via|Viale|Piazza|Corso|C\.so|V\.le|Str\.)\s+[A-Za-zÀ-ÿ\s]+?\s*\d{1,5}(?:\s*[A-Za-z]?)?)',
             r'DATI FORNITURA.*?VIA\s(.*?\d{5}\s\w{2})',
+            r'(?:DATI FORNITURA|Indirizzo|Luogo di fornitura|Servizio erogato in|Ubicazione).*?VIA[\s\n]+(.*?\d{5}\s\w{2})',
         ]
         
         for pattern in patterns:

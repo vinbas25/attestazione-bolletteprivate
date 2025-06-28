f4cc27fcca1f3af3821fc2bb9d51fa6e4ca36206
vinbas25 <vincenzo.basile.unina2@gmail.com>
2025-06-28 11:49:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -564,7 +564,7 @@ def crea_attestazione(dati: List[Dict[str, str]], firma_selezionata: str = "Mar.
             "Si dichiara che i costi riportati nelle seguenti fatture elettroniche:\n"
         )
 
-        specific_addresses = ["XXXX", "YYYY"]
+        specific_addresses = ["garibaldi", "YYYY"]
         address_present = any(address in dati[0].get('Indirizzo', '') for address in specific_addresses)
 
         if address_present:

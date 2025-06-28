dd7138c7002be03f0a798d5699077e649f1b5faa
vinbas25 <vincenzo.basile.unina2@gmail.com>
2025-06-28 17:39:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -336,7 +336,7 @@ def determina_tipo_bolletta(societa: str, testo: str) -> str:
             return "gas"
         else:
             return "energia"
-    if any(kw in societa_lower for kw in ["acqua", "acquedotto", "fiora", "nuove acque", "pubbliacqua", "gaia", "acque", "asa"]):
+    if any(kw in societa_lower for kw in ["acqua", "acquedotto", "fiora", "nuove acque", "pubbliacqua", "gaia", "acque", "asa", "GEAL"]):
         return "acqua"
     elif any(kw in societa_lower for kw in ["energia", "enel", "a2a", "edison"]):
         return "energia"
